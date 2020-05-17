85e6504705aac857d7a60bfab102caecd9eda673
RookHeaven <romagra92@yandex.ru>
2020-05-17 20:54:18 +0500

    Класс "Фигура"

--- Snake/Snake/VerticalLine.cs
+++ Snake/Snake/VerticalLine.cs
@@ -6,24 +6,15 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class VerticalLine
+    class VerticalLine : Figure
     {
-        List<Point> pList;
-
-        public VerticalLine(int x, int yLeft, int yRight, char sym)
+        public VerticalLine(int x, int yUp, int yDown, char sym)
         {
             pList = new List<Point>();
-            for (int y = yLeft; y <= yRight; y++)
+            for (int y = yUp; y <= yDown; y++)
             {
                 Point p = new Point(x, y, sym);
                 pList.Add(p);
-            }
-        }
-        public void Draw()
-        {
-            foreach (Point p in pList)
-            {
-                p.Draw();
             }
         }
     }

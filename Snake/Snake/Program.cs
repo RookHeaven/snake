d7e85524e4fde17e2c803acae54dfe2d489562ba
RookHeaven <romagra92@yandex.ru>
2020-05-23 16:15:58 +0500

    Итоги. Конец игры

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -9,30 +9,30 @@ namespace Snake
 {
     class Program
     {
-        static void Main(string[] args)
+		static void Main(string[] args)
 		{
 
-			Console.SetBufferSize( 80, 25 );
+			Console.SetBufferSize(80, 25);
 
-				Walls walls = new Walls(80, 25);
+			Walls walls = new Walls(80, 25);
 			walls.Draw();
 
-				// Отрисовка точек			
-				Point p = new Point(4, 5, '*');
+			// Отрисовка точек			
+			Point p = new Point(4, 5, '*');
 			Snake snake = new Snake(p, 4, Direction.RIGHT);
 			snake.Draw();
 
-				FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+			FoodCreator foodCreator = new FoodCreator(80, 25, '$');
 			Point food = foodCreator.CreateFood();
 			food.Draw();
 
 			while (true)
 			{
-				if (walls.IsHit(snake) || snake.IsHitTail() )
+				if (walls.IsHit(snake) || snake.IsHitTail())
 				{
 					break;
 				}
-				if(snake.Eat(food ) )
+				if (snake.Eat(food))
 				{
 					food = foodCreator.CreateFood();
 					food.Draw();
@@ -42,13 +42,35 @@ namespace Snake
 					snake.Move();
 				}
 
-				Thread.Sleep( 100 );
+				Thread.Sleep(100);
 				if (Console.KeyAvailable)
 				{
 					ConsoleKeyInfo key = Console.ReadKey();
 					snake.HandleKey(key.Key);
 				}
 			}
+			WriteGameOver();
+			Console.ReadLine();
+		}	
+
+		static void WriteGameOver()
+		{
+			int xOffset = 25;
+			int yOffset = 8;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.SetCursorPosition(xOffset, yOffset++);
+			WriteText("============================", xOffset, yOffset++);
+			WriteText("И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++);
+			yOffset++;
+			WriteText("Автор: Роман Грачев", xOffset + 4, yOffset++);
+			WriteText("Специально для GeekBrains", xOffset + 1, yOffset++);
+			WriteText("============================", xOffset, yOffset++);
+		}
+
+		static void WriteText(String text, int xOffset, int yOffset)
+		{
+			Console.SetCursorPosition(xOffset, yOffset);
+			Console.WriteLine(text);
 		}
 	}
 }
